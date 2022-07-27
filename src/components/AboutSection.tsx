8472a73db8b169d93a6249a8b20beb1a900d9f0b
Edison Liem <edisonliem417@gmail.com>
2022-07-27 13:41:18 -0400

    Fix the lagging issue in the about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -14,20 +14,20 @@ const AboutSection: React.FC = () => {
     return (
         <section className='about-section-container' style={{backgroundImage: `url(${TechnologyAnimation})`}} >
             <div className="white-board" ref={whiteBoardRef} >
-                <div className="about-us-container">
-                    <Fade bottom cascade>
+                <Fade bottom cascade>
+                    <div className="about-us-container">
                         <h3>About Us</h3>
                         <p className='description'>
                             FinTech Society (FTS) serves as a platform to connect tech-savvy and finance enthusiasts from all disciplines. Through a series of workshops, peer-mentorship, case competitions, career exploration and guest speaker events, we hope to foster the learning of financial technology in areas such as BlockChain, Algo Trading, Cryptocurrency and Artificial Intelligence. Our goal is to equip students with knowledge of financial technology and provide valuable resources to students who want to step foot into the industry. 
                         </p>
-                    </Fade>
-                </div>
-                <div className="join-us-container">
-                    <Fade bottom cascade>
-                    <h3>Join Our Membership Now</h3>
-                    <button>Join Us</button>
-                    </Fade>
-                </div>
+                    </div>
+                </Fade>
+                <Fade bottom cascade>
+                    <div className="join-us-container">
+                        <h3>Join Our Membership Now</h3>
+                        <button>Join Us</button>
+                    </div>
+                </Fade>
             </div>
         </section>
     )
