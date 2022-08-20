d48bbdaa504b92773df552efee31d015943a3ad3
Edison Liem <edisonliem417@gmail.com>
2022-08-20 15:15:13 -0400

    Change hero section background

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ const Fade = require('react-reveal/Fade');
 
 const HeroSection: React.FC = () => {
     return (
-        <section className='hero-section-container' style={{backgroundImage: `linear-gradient(rgba(50,50,50,.5), rgba(50,50,50,.6)), url(${HeroSectionBackground})`}}>
+        <section className='hero-section-container' style={{backgroundImage: `linear-gradient(rgba(50,50,50,.6), rgba(50,50,50,.6)), url(${HeroSectionBackground})`}}>
             <Fade bottom cascade>
                 <div className="hero-text-container">
                     <h2 className='welcome-text'>Welcome to</h2>
