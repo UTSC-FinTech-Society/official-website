79ee0d2ad77c716c81fe3756719d58c4e79b7501
Edison Liem <edisonliem417@gmail.com>
2022-08-21 22:59:29 -0400

    Add new team executive to the team section

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -7,6 +7,7 @@ import CassieXu from "../assets/team/cassie-xu.png";
 import EdisonLiem from '../assets/team/edison-liem.png';
 import DarrenCheng from "../assets/team/darren-cheng.jpg";
 import DionChi from "../assets/team/dion-chi.jpg";
+import ElsonLeung from "../assets/team/elson-leung.jpg";
 import GloriaWu from "../assets/team/gloria-wu.jpeg";
 import JekyllChen from "../assets/team/jekyll-chen.jpeg";
 import RyanMok from "../assets/team/ryan-mok.jpeg";
@@ -35,6 +36,7 @@ const TeamSection: React.FC = () => {
                 </TeamGroup>
                 <TeamGroup name='Directors' >
                     <ExecutiveCard name='Dion Chi' position='Director of Operations' profilePic={DionChi}  />
+                    <ExecutiveCard name='Elson Leung' position='Director of Operations' profilePic={ElsonLeung}  />
                     <ExecutiveCard name='Gloria Wu' position='Director of Finance' profilePic={GloriaWu}  />
                     <ExecutiveCard name='Jekyll Chen' position='Director of Marketing' profilePic={JekyllChen}  />
                     <ExecutiveCard name='Ryan Mok' position='Director of External Affairs' profilePic={RyanMok}  />
