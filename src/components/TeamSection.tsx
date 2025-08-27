933b7c3711122d8237b803b79778782dac0aaf41
Edison Liem <edisonliem417@gmail.com>
2025-08-27 17:20:24 -0400

    Renew team section to 2024-2025 team images

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -1,35 +1,22 @@
 import React, { useRef } from 'react';
 import ExecutiveCard from './ExecutiveCard';
 import TeamGroup from './TeamGroup';
-import KyleLam from '../assets/team/kyle-lam.jpg';
-import AarushiDoshi from '../assets/team/aarushi-doshi.jpeg';
-import SiyaGrover from '../assets/team/siya-grover.jpg';
-import EdisonLiem from '../assets/team/edison-liem.png';
-import KatelynTam from '../assets/team/katelyn-tam.png';
-import JanneyTran from '../assets/team/janney-tran.jpeg';
-import SurachartCharaspitayakul from '../assets/team/surachart-charaspitayakul.jpg';
-import SyedMuntasirTashdid from '../assets/team/syed-muntasir-tashdid.jpeg';
-import SanskritiKanagala from '../assets/team/sanskriti-kanagala.jpg';
-import AniketDuggal from '../assets/team/aniket-duggal.png';
-import AddyArulkumar from '../assets/team/addy-arulkumar.png';
-import OmarElOud from '../assets/team/omar-el-oud.jpeg';
-import SiddhiSuranaMahaveer from '../assets/team/siddhi-surana-mahaveer.jpeg';
-import AmanVishwanath from '../assets/team/aman-vishwanath.jpg';
-import KatherineChen from '../assets/team/katherine-chen.jpeg';
-import NikiAlizadehSaghati from '../assets/team/niki-alizadeh-saghati.jpg';
-import JovitaAu from '../assets/team/jovita-au.jpg';
-import BrianJiang from '../assets/team/brian-jiang.jpg';
-import GabrielBarwicz from '../assets/team/gabriel-barwicz.jpeg';
-import RobertFung from '../assets/team/robert-fung.png';
-import AaravRoyChowdhury from '../assets/team/aarav-roy-chowdhury.jpg';
-import BrianNg from '../assets/team/brian-ng.jpg';
-import JessicaZhu from '../assets/team/jessica-zhu.jpeg';
-import MorrisHo from '../assets/team/morris-ho.jpg';
-import ArishaAhmed from '../assets/team/arisha-ahmed.jpg';
-import AmnaTariq from '../assets/team/amna-tariq.png';
-import SaumyaSoni from '../assets/team/saumya-soni.jpg';
-import AaryaPrakash from '../assets/team/aarya-prakash.jpg';
-import MuhammadIrtiza from '../assets/team/muhammad-irtiza.jpg';
+import JanneyTran from '../assets/team/2024-2025/janney-tran.jpeg';
+import KyleLam from '../assets/team/2024-2025/kyle-lam.jpg';
+import EdisonLiem from '../assets/team/2024-2025/edison-liem.png';
+import KatelynTam from '../assets/team/2024-2025/katelyn-tam.png';
+import SurachartCharaspitayakul from '../assets/team/2024-2025/surachart-charaspitayakul.jpg';
+import YaminahMeka from '../assets/team/2024-2025/yaminah-meka.jpg';
+import NikiAlizadehSaghati from '../assets/team/2024-2025/niki-alizadeh-saghati.jpg';
+import JuhiShah from '../assets/team/2024-2025/juhi-shah.jpeg';
+import EthanZhao from '../assets/team/2024-2025/ethan-zhao.png';
+import JaceLu from '../assets/team/2024-2025/jace-lu.jpg';
+import VincePersaud from '../assets/team/2024-2025/vince-persaud.jpg';
+import IshikaMadanpotra from '../assets/team/2024-2025/ishika-madanpotra.jpg';
+import PeetishaMangal from '../assets/team/2024-2025/peetisha-mangal.jpeg';
+import PriscillaSimanjuntak from '../assets/team/2024-2025/priscilla-simanjuntak.png';
+import RingoWu from '../assets/team/2024-2025/ringo-wu.jpg';
+import HannahYang from '../assets/team/2024-2025/hannah-yang.jpg';
 
 const TeamSection: React.FC = () => {
     const whiteBoardRef = useRef<HTMLDivElement>(null);
@@ -44,44 +31,28 @@ const TeamSection: React.FC = () => {
        <section className='team-section-container'>
             <div className="white-board" ref={whiteBoardRef} >
                 <TeamGroup name='Presidents' >
+                    <ExecutiveCard name='Janney Tran' position='Co-President' profilePic={JanneyTran}  />
                     <ExecutiveCard name='Kyle Lam' position='Co-President' profilePic={KyleLam}  />
-                    <ExecutiveCard name='Aarushi Doshi' position='Co-President' profilePic={AarushiDoshi}  />
                 </TeamGroup>
                 <TeamGroup name='Vice Presidents' >
-                
-                    <ExecutiveCard name='Siya Grover' position='Vice President of Finance' profilePic={SiyaGrover}  />
                     <ExecutiveCard name='Edison Liem' position='Vice President of Technology' profilePic={EdisonLiem}  />
                     <ExecutiveCard name='Katelyn Tam' position='Vice President of Research' profilePic={KatelynTam}  />
-                    <ExecutiveCard name='Janney Tran' position='Vice President of Operations' profilePic={JanneyTran}  />
                     <ExecutiveCard name='Surachart Charaspitayakul' position='Vice President of Research' profilePic={SurachartCharaspitayakul}  />
-                    <ExecutiveCard name='Syed Muntasir Tashdid' position='Vice President of Business Development' profilePic={SyedMuntasirTashdid}  />
-                    <ExecutiveCard name='Sanskriti Kanagala' position='Vice President of Marketing' profilePic={SanskritiKanagala}  />
+                    <ExecutiveCard name='Yaminah Meka' position='Vice President of External Affairs' profilePic={YaminahMeka}  />
+                    <ExecutiveCard name='Niki Alizadeh Saghati' position='Vice President of Operations and Finance' profilePic={NikiAlizadehSaghati}  />
                 </TeamGroup>
                 <TeamGroup name='Directors' >
-                    <ExecutiveCard name='Aniket Duggal' position='Director of Operations' profilePic={AniketDuggal}  />
-                    <ExecutiveCard name='Addy Arulkumar' position='Director of Operations' profilePic={AddyArulkumar}  />
-                    <ExecutiveCard name='Omar El Oud' position='Director of Finance' profilePic={OmarElOud}  />
-                    <ExecutiveCard name='Siddhi Surana Mahaveer' position='Director of Finance' profilePic={SiddhiSuranaMahaveer}  />
-                    <ExecutiveCard name='Aman Vishwanath' position='Director of Business Development' profilePic={AmanVishwanath}  />
-                    <ExecutiveCard name='Katherine Chen' position='Director of Finance' profilePic={KatherineChen}  />
-                    <ExecutiveCard name='Niki Alizadeh Saghati' position='Director of Marketing' profilePic={NikiAlizadehSaghati}  />
-                    <ExecutiveCard name='Jovita Au' position='Director of Marketing' profilePic={JovitaAu}  />
-                </TeamGroup>
-                <TeamGroup name='Research Team' >
-                    <ExecutiveCard name='Brian Jiang' position='Analyst' profilePic={BrianJiang}  />
-                    <ExecutiveCard name='Gabriel Barwicz' position='Analyst' profilePic={GabrielBarwicz}  />
-                    <ExecutiveCard name='Robert Fung' position='Analyst' profilePic={RobertFung}  />
-                    <ExecutiveCard name='Aarav Roy Chowdhury' position='Analyst' profilePic={AaravRoyChowdhury}  />
-                    <ExecutiveCard name='Brian Ng' position='Analyst' profilePic={BrianNg}  />
-                    <ExecutiveCard name='Jessica Zhu' position='Analyst' profilePic={JessicaZhu}  />
+                    <ExecutiveCard name='Juhi Shah' position='Director of Business Development' profilePic={JuhiShah}  />
+                    <ExecutiveCard name='Ethan Zhao' position='Director of Business Development' profilePic={EthanZhao}  />
+                    <ExecutiveCard name='Jace Lu' position='Director of Operations' profilePic={JaceLu}  />
+                    <ExecutiveCard name='Vince Persaud' position='Director of Operations' profilePic={VincePersaud}  />
+                    <ExecutiveCard name='Ishika Madanpotra' position='Director of Marketing' profilePic={IshikaMadanpotra}  />
+                    <ExecutiveCard name='Peetisha Mangal' position='Director of Marketing' profilePic={PeetishaMangal}  />
                 </TeamGroup>
                 <TeamGroup name='Associates' >
-                    <ExecutiveCard name='Morris Ho' position='Developer' profilePic={MorrisHo}  />
-                    <ExecutiveCard name='Arisha Ahmed' position='Operations Associate' profilePic={ArishaAhmed}  />
-                    <ExecutiveCard name='Amna Tariq' position='Finance Associate' profilePic={AmnaTariq}  />
-                    <ExecutiveCard name='Saumya Soni' position='Business Development Associate' profilePic={SaumyaSoni}  />
-                    <ExecutiveCard name='Aarya Prakash' position='Developer' profilePic={AaryaPrakash}  />
-                    <ExecutiveCard name='Muhammad Irtiza' position='Business Development Associate' profilePic={MuhammadIrtiza}  />
+                    <ExecutiveCard name='Priscilla Simanjuntak' position='Research Analyst' profilePic={PriscillaSimanjuntak}  />
+                    <ExecutiveCard name='Ringo Wu' position='Financial Analyst' profilePic={RingoWu}  />
+                    <ExecutiveCard name='Hannah Yang' position='Financial Analyst' profilePic={HannahYang}  />
                 </TeamGroup>
             </div>
        </section>
